16a8688d0568830a67bb640f96a398f4914e5034
paucebr <paucebr@gmail.com>
2016-10-14 15:39:37 +0200

    estoy en el cvc

--- week1/peaks_extractor.m
+++ week1/peaks_extractor.m
@@ -1,11 +1,11 @@
 %%Function used to extract the local maxima (peaks) from histograms. Later
 %%these peaks will be used to find the threshold
-function [locs,locs_smooth] = peaks_extractor (yHist)
+function [locs_smooth] = peaks_extractor (yHist, smooth)
     [pks,locs] = findpeaks(yHist); %returns a vector with the local maxima (peaks).
     
     
     yHist_smooth=yHist;
-    for z=1:10
+    for z=1:smooth
         yHist_smooth=smooth(yHist_smooth); %smooths the data in the column vector using a moving average filter
     end
     [pks_smooth,locs_smooth] = findpeaks(yHist_smooth);
