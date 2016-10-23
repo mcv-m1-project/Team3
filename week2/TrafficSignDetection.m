b5884c169297519fb16589dacb55279e97cf2729
Guillem Cucurull <gcucurull@gmail.com>
2016-10-23 18:20:14 +0200

    generate test images

--- week2/TrafficSignDetection.m
+++ week2/TrafficSignDetection.m
@@ -68,18 +68,7 @@ function TrafficSignDetection(directory, pixel_method, window_method, decision_m
         % Candidate Generation (pixel) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         pixelCandidates = CandidateGenerationPixel_Color(im, pixel_method);
         
-        pixelCandidates_old=pixelCandidates;
         pixelCandidates = task3(pixelCandidates, pixel_method, element);
-        
-        
-       
-%         pause(.0001);
-%         subplot(1,2,1);
-%         imshow(pixelCandidates_old);
-%         title('Original')
-%         subplot(1,2,2);
-%         imshow(pixelCandidates);
-%         title('Modified')
         
         % Candidate Generation (window)%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % windowCandidates = CandidateGenerationWindow_Example(im, pixelCandidates, window_method); %%'SegmentationCCL' or 'SlidingWindow'  (Needed after Week 3)
---
+++ week2/TrafficSignDetection_test.m
@@ -0,0 +1,187 @@
+%
+% Template example for using on the test set (no annotations).
+% 
+ 
+function TrafficSignDetection_validation(input_dir, output_dir, pixel_method, window_method, decision_method)
+    % TrafficSignDetection
+    % Perform detection of Traffic signs on images. Detection is performed first at the pixel level
+    % using a color segmentation. Then, using the color segmentation as a basis, the most likely window 
+    % candidates to contain a traffic sign are selected using basic features (form factor, filling factor). 
+    % Finally, a decision is taken on these windows using geometric heuristics (Hough) or template matching.
+    %
+    %    Parameter name      Value
+    %    --------------      -----
+    %    'input_dir'         Directory where the test images to analize  (.jpg) reside
+    %    'output_dir'        Directory where the results are stored
+    %    'pixel_method'      Name of the color space: 'opp', 'normrgb', 'lab', 'hsv', etc. (Weeks 2-5)
+    %    'window_method'     'SegmentationCCL' or 'SlidingWindow' (Weeks 3-5)
+    %    'decision_method'   'GeometricHeuristics' or 'TemplateMatching' (Weeks 4-5)
+
+
+    global CANONICAL_W;        CANONICAL_W = 64;
+    global CANONICAL_H;        CANONICAL_H = 64;
+    global SW_STRIDEX;         SW_STRIDEX = 8;
+    global SW_STRIDEY;         SW_STRIDEY = 8;
+    global SW_CANONICALW;      SW_CANONICALW = 32;
+    global SW_ASPECTRATIO;     SW_ASPECTRATIO = 1;
+    global SW_MINS;            SW_MINS = 1;
+    global SW_MAXS;            SW_MAXS = 2.5;
+    global SW_STRIDES;         SW_STRIDES = 1.2;
+
+
+    % Load models
+    %global circleTemplate;
+    %global givewayTemplate;   
+    %global stopTemplate;      
+    %global rectangleTemplate; 
+    %global triangleTemplate;  
+    %
+    %if strcmp(decision_method, 'TemplateMatching')
+    %   circleTemplate    = load('TemplateCircles.mat');
+    %   givewayTemplate   = load('TemplateGiveways.mat');
+    %   stopTemplate      = load('TemplateStops.mat');
+    %   rectangleTemplate = load('TemplateRectangles.mat');
+    %   triangleTemplate  = load('TemplateTriangles.mat');
+    %end
+
+    files = ListFiles(input_dir);
+    
+    mkdir(strcat(output_dir, '/test/'));
+    
+    for ii=1:size(files,1),
+
+        ii
+        
+        % Read file
+        im = imread(strcat(input_dir,'/',files(ii).name));
+     
+        % Candidate Generation (pixel) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        pixelCandidates = CandidateGenerationPixel_Color(im, pixel_method);
+        pixelCandidates = task3(pixelCandidates, pixel_method, element);
+        
+        
+        % Candidate Generation (window)%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % windowCandidates = CandidateGenerationWindow_Example(im, pixelCandidates, window_method); %%'SegmentationCCL' or 'SlidingWindow'  (Needed after Week 3)
+
+        out_file1 = sprintf ('%s/test/pixelCandidates_%06d.png',  output_dir, ii);
+	    %out_file2 = sprintf ('%s/test/windowCandidates_%06d.mat', output_dir, ii);
+
+	    imwrite (pixelCandidates, out_file1);
+	    %save (out_file2, 'windowCandidates');        
+    end
+end
+ 
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CandidateGeneration
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [pixelCandidates] = CandidateGenerationPixel_Color(im, space)
+
+    im=double(im);
+
+     switch space
+        case 'rgb'
+            r_th = [11.8967, 56.8533, 52.7663];
+            red_pixelCandidates = im(:,:,1) > r_th(1) & im(:,:,2) < r_th(2) & im(:,:,3) < r_th(3);
+            
+            b_th = [44.1466, 60.4712, 24.5236];
+            blue_pixelCandidates = im(:,:,1) < b_th(1) & im(:,:,2) < b_th(2) & im(:,:,3) > b_th(3);
+            
+            pixelCandidates = red_pixelCandidates | blue_pixelCandidates;
+            
+        case 'normrgb'
+            % normalize rgb
+            im = NormRGB(double(im));
+            
+            r_th = [0.0830    0.2943    0.2870];
+            red_pixelCandidates = im(:,:,1) > r_th(1) & im(:,:,2) < r_th(2) & im(:,:,3) < r_th(3);
+            
+            b_th = [0.2625    0.3121    0.1951];
+            blue_pixelCandidates = im(:,:,1) < b_th(1) & im(:,:,2) < b_th(2) & im(:,:,3) > b_th(3);
+            
+            pixelCandidates = red_pixelCandidates | blue_pixelCandidates;
+        
+        case 'hsv'
+            imhsv = rgb2hsv(im);
+            im_h = imhsv(:,:,1);
+            im_s = imhsv(:,:,2);
+            im_v = imhsv(:,:,3);
+            
+            v_th = [0.1*255 1*255];
+            
+            r_th = [0.96    0.04     0.5];
+            red_pixelCandidates = (im_h > r_th(1) | im_h < r_th(2)) & im_s > r_th(3);
+            
+            b_th = [0.56    0.76     0.5];
+            blue_pixelCandidates = im_h > b_th(1) & im_h < b_th(2) & im_s > b_th(3);
+            
+            pixelCandidates = red_pixelCandidates | blue_pixelCandidates;
+            
+            pixelCandidates = pixelCandidates & im_v > v_th(1) & im_v < v_th(2);
+            
+        case 'lab'
+            imlab = rgb2lab(im);
+                        
+            im_a = imlab(:,:,2);
+            im_b = imlab(:,:,3);
+            
+            red_pixelCandidates = im_a > 0;
+            blue_pixelCandidates = im_b < 0;
+            
+            pixelCandidates = red_pixelCandidates | blue_pixelCandidates;
+        
+         case 'histEq'
+            im = histogramEqualization(im);
+            
+            r_th = [0.1757    0.7075    0.6895];
+            red_pixelCandidates = im(:,:,1) > r_th(1) & im(:,:,2) < r_th(2) & im(:,:,3) < r_th(3);
+            
+            b_th = [0.5784    0.6954    0.4534];
+            blue_pixelCandidates = im(:,:,1) < b_th(1) & im(:,:,2) < b_th(2) & im(:,:,3) > b_th(3);
+            
+            pixelCandidates = red_pixelCandidates | blue_pixelCandidates;
+         
+         case 'hbp'
+            imhsv = rgb2hsv(im);
+            im_h = imhsv(:,:,1);
+            im_s = imhsv(:,:,2);
+            
+            bins = 32;
+            
+            load(['red_hist_' num2str(bins) '.mat']);
+            load(['blue_hist_' num2str(bins) '.mat']);
+            load(['rb_hist_' num2str(bins) '.mat']);
+            
+            red_hist(:,1:round(bins*0.5)) = 0;
+            blue_hist(:,1:round(bins*0.5)) = 0;
+            rb_hist(:,1:round(bins*0.5)) = 0;
+  
+            pixels = [im_h(:) im_s(:)];
+            pixels = ceil(pixels*bins); % from pixels to bins
+            pixels(pixels==0) = 1; % convert 0 to ones because the loswest bin is 1.
+
+            pixelCandidates = zeros(size(im_h));
+            pixelCandidates = reshape(pixelCandidates, [size(pixelCandidates, 1)*size(pixelCandidates, 2), 1]);
+
+            for p=1:size(pixelCandidates, 1)
+                hist_i = pixels(p,1);
+                hist_j = pixels(p,2);
+                pixelCandidates(p) = (red_hist(hist_i, hist_j) > 0.005) | (blue_hist(hist_i, hist_j) > 0.005) | (rb_hist(hist_i, hist_j) > 0.005);
+            end
+            pixelCandidates = reshape(pixelCandidates, size(im_h));
+            
+        otherwise
+            error('Incorrect color space defined');
+            return
+    end
+end    
+    
+
+function [windowCandidates] = CandidateGenerationWindow_Example(im, pixelCandidates, window_method)
+    windowCandidates = [ struct('x',double(12),'y',double(17),'w',double(32),'h',double(32)) ];
+end  
