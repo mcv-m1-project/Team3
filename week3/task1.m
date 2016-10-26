e19f0e63beb429b1b3fa0376f6ed91ce4e8e27f7
Arnau <arbamas@gmail.com>
2016-10-26 18:47:19 +0200

    struct window candidates

--- week3/task1.m
+++ week3/task1.m
@@ -1,15 +1,15 @@
 %%
 %1st approach: Connected Component Labeling
-function [BoundingBoxList] = task1 (pixelCandidates)
+function [windowCandidates] = task1 (pixelCandidates)
     BB = regionprops(pixelCandidates,'BoundingBox','FilledArea');
-    BoundingBoxList=[];
+    windowCandidates=[];
     imshow(pixelCandidates)
     hold on;
     for i=1:size(BB,1)
         %conditions extracted to analysis of task1 of the week1
         if (BB(i).BoundingBox(3)*BB(i).BoundingBox(4))>850 && (BB(i).BoundingBox(3)*BB(i).BoundingBox(4))<55850 &&((BB(i).FilledArea*100)/(BB(i).BoundingBox(3)*BB(i).BoundingBox(4))) > 0.4 && BB(i).BoundingBox(3)/BB(i).BoundingBox(4)>0.6  && BB(i).BoundingBox(3)/BB(i).BoundingBox(4)<2.4
             rectangle('Position',[BB(i).BoundingBox(1),BB(i).BoundingBox(2),BB(i).BoundingBox(3),BB(i).BoundingBox(4)],'EdgeColor','y');
-            BoundingBoxList=[BoundingBoxList; BB(i).BoundingBox(:)'];
+            windowCandidates = [ windowCandidates;struct('x',double(BB(i).BoundingBox(1)),'y',double(BB(i).BoundingBox(2)),'w',double(BB(i).BoundingBox(3)),'h',double(BB(i).BoundingBox(4))) ];
         end
     end
 end
