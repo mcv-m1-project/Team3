2804dd617572c6a623f42d3b7ef25157c2e35a2d
Guillem Cucurull <gcucurull@gmail.com>
2016-10-28 18:53:56 +0200

    better morf and max nms

--- week3/morf.m
+++ week3/morf.m
@@ -1,11 +1,11 @@
 function [pixelCandidates] = morf( pixelCandidates, element)
     
-%     ee=strel('line',7,0);
-%     pixelCandidates = imclose(pixelCandidates,ee);
-%     ee=strel('line',7,60);
-%     pixelCandidates = imclose(pixelCandidates,ee);
-%     ee=strel('line',7,-60);
-%     pixelCandidates = imclose(pixelCandidates,ee);
+    ee=strel('line',7,0);
+    pixelCandidates = imclose(pixelCandidates,ee);
+    ee=strel('line',7,60);
+    pixelCandidates = imclose(pixelCandidates,ee);
+    ee=strel('line',7,-60);
+    pixelCandidates = imclose(pixelCandidates,ee);
 % 
 %     ee=strel('line', 20, 0);
 %     pixelCandidates = imdilate(pixelCandidates,ee);
@@ -19,15 +19,8 @@ function [pixelCandidates] = morf( pixelCandidates, element)
     pixelCandidates = imfill(pixelCandidates,'holes');
     %remove noise ---> Opening+Closing = [(erode+dilate)+(dilate+erode)]
             
-    %ee=strel('diamond',6);
     pixelCandidates = imopen(pixelCandidates,element);
     pixelCandidates = imclose(pixelCandidates,element);
-    
-
-    
-     %pixelCandidates = imfill(pixelCandidates,'holes');
-    
- %   pixelCandidates = xor(bwareaopen(pixelCandidates,800),  bwareaopen(pixelCandidates,20000));
     
 end
 
