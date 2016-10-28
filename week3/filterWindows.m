e51167df6b2ac02bac448b1794fef0b299f9320a
Guillem Cucurull <gcucurull@gmail.com>
2016-10-28 20:35:50 +0200

    Try params

--- week3/filterWindows.m
+++ week3/filterWindows.m
@@ -3,10 +3,10 @@ function [ out_windows ] = filterWindows( in_windows )
 %   Detailed explanation goes here
 out_windows = [];
 for i=1:size(in_windows,1)
-    size = in_windows(i).w * in_windows(i).h;
+    sizew = in_windows(i).w * in_windows(i).h;
     aspectRatio = in_windows(i).w / in_windows(i).h;
     
-    if(size < 56000 && size >  900 && aspectRatio > 0.7 && aspectRatio < 2.3)
+    if(sizew < 56000 && sizew >  900 && aspectRatio > 0.7 && aspectRatio < 2.3)
        out_windows = [out_windows; in_windows(i)]; 
     end
 end
