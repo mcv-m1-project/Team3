abee2f83e5d88fe0849d908b1003d68861618506
Arnau <arbamas@gmail.com>
2016-11-02 14:54:16 +0100

    a

--- week4/edgesDetection.m
+++ week4/edgesDetection.m
@@ -1,44 +1,51 @@
 function [ BW ] = edgesDetection( I, edges, color)
-    switch color
-        case 'bw'
-            I=im2bw(I);
-        case 'bwOtsu'
-            level = graythresh(I);
-            I = im2bw(I,level);
-        case 'gray'
-            I=rgb2gray(I);
-        otherwise
-            error('Color name not valid.');
-    end
-    
-    switch edges
-        case 'Sobel'
-            BW = edge(I,'Sobel');
-        case 'Prewitt'
-            BW = edge(I,'Prewitt');
-        case 'Canny'
-            BW = edge(I,'Canny');
-        case 'Roberts'
-            BW = edge(I,'Roberts');
-        case 'log'
-            BW = edge(I,'log');
-        case 'gradeMagnitud'
-          [Gx, Gy] = imgradientxy(I);
-          [BW, Gdir] = imgradient(Gx, Gy);
-        case 'gradeMagnitudSobel'
-          [BW,Gdir] = imgradient(I,'sobel');
-        case 'gradeMagnitudPrewitt'
-          [BW,Gdir] = imgradient(I,'prewitt') ;
-        case 'gradeMagnitudCentral'
-          [BW,Gdir] = imgradient(I,'central') ;
-        case 'gradeMagnitudIntermediate'
-          [BW,Gdir] = imgradient(I,'intermediate');
-        case 'gradeMagnitudRoberts'
-          [BW,Gdir] = imgradient(I,'roberts');
-        otherwise
-           error('Edges detector method not valid.');
-    end    
-        
+switch color
+    case 'bw'
+        I=im2bw(I);
+    case 'bwOtsu'
+        level = graythresh(I);
+        I = im2bw(I,level);
+    case 'gray'
+        I=rgb2gray(I);
+    otherwise
+        error('Color name not valid.');
+end
+
+switch edges
+    case 'Sobel'
+        BW = edge(I,'Sobel');
+    case 'Prewitt'
+        BW = edge(I,'Prewitt');
+    case 'Canny'
+        BW = edge(I,'Canny');
+    case 'Roberts'
+        BW = edge(I,'Roberts');
+    case 'log'
+        BW = edge(I,'log');
+    case 'gradeMagnitud'
+        [Gx, Gy] = imgradientxy(I);
+        [BW, Gdir] = imgradient(Gx, Gy);
+    case 'gradeMagnitudSobel'
+        [BW,Gdir] = imgradient(I,'sobel');
+    case 'gradeMagnitudPrewitt'
+        [BW,Gdir] = imgradient(I,'prewitt') ;
+    case 'gradeMagnitudCentral'
+        [BW,Gdir] = imgradient(I,'central') ;
+    case 'gradeMagnitudIntermediate'
+        [BW,Gdir] = imgradient(I,'intermediate');
+    case 'gradeMagnitudRoberts'
+        [BW,Gdir] = imgradient(I,'roberts');
+    case 'gradeMagnitudMorpho'
+        ee=strel('diamond',2);
+        %I=imopen(I,ee);   %contours not closed
+        [Gx, Gy] = imgradientxy(I);
+        [BW, Gdir] = imgradient(Gx, Gy);
+        BW = imclose(BW,ee); %remove noise
+        BW = bwmorph(BW,'remove'); %thinning
+    otherwise
+        error('Edges detector method not valid.');
+end
+
 
 
 
